96111d6fb3be1c8c854fabfa92a7347ab37b540f
okomojacob <jaylabs2030@gmail.com>
2021-03-12 23:02:05 +0300

    os.pth.join

---
+++ LSTM/intntnlAirlinePassengersPrediction-LSTM/passengersLstm.py
@@ -0,0 +1,12 @@
+# Import the required libraries
+from matplotlib import pyplot as plt
+import pandas as pd
+import os
+
+print(os.getcwd())
+path = os.path.join
+"""
+dataset = pd.read_csv('airPassengers.csv', usecols=[1], engine='python')
+plt.plot(dataset)
+plt.show()
+"""
