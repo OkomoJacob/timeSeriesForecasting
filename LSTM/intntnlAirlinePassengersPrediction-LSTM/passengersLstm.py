23b3be936f05016cc6b672d5e6f5c56da12eda91
okomojacob <jaylabs2030@gmail.com>
2021-03-12 23:38:12 +0300

    code  edits

--- LSTM/intntnlAirlinePassengersPrediction-LSTM/passengersLstm.py
+++ LSTM/intntnlAirlinePassengersPrediction-LSTM/passengersLstm.py
@@ -1,8 +1,9 @@
-# Import the required libraries
+# Import all the required python libraries
 from matplotlib import pyplot as plt
 import pandas as pd
 import os
 
+# Load the data, from the local dir
 data = pd.read_csv('data/oldPassengers.csv', usecols=[1], engine='python')
 plt.plot(data)
 plt.show()
