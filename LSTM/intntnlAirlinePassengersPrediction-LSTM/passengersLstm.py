bf3ec3a99b84355831f4ae6ab11311183dcf7ee5
okomojacob <jaylabs2030@gmail.com>
2021-03-12 23:46:56 +0300

    built with

--- LSTM/intntnlAirlinePassengersPrediction-LSTM/passengersLstm.py
+++ LSTM/intntnlAirlinePassengersPrediction-LSTM/passengersLstm.py
@@ -6,4 +6,5 @@ import os
 # Load the data, from the local dir
 data = pd.read_csv('data/oldPassengers.csv', usecols=[1], engine='python')
 plt.plot(data)
+plt.title("A Raw plot of our Data")
 plt.show()
