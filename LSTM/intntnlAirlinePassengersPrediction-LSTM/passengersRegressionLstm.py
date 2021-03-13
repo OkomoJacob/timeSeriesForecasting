93d3a33d28da545d7b6ac95df9efdc77015aea3d
okomojacob <jaylabs2030@gmail.com>
2021-03-13 10:25:17 +0300

    Code update, again

--- LSTM/intntnlAirlinePassengersPrediction-LSTM/passengersRegressionLstm.py
+++ LSTM/intntnlAirlinePassengersPrediction-LSTM/passengersRegressionLstm.py
@@ -1,4 +1,6 @@
-# Import the necessary libraries,ffunctions and classes
+"""
+STEP 1: Import the necessary libraries,ffunctions and classes
+"""
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 from keras.models import Sequential
@@ -9,13 +11,20 @@ import pandas as pd
 import numpy as np
 import math
 
+"""
+STEP 2: Data preprocessing
+"""
 # For reproduciblity, fix the random number seed.
 np.random.seed(7)
 
-# load the passenger dataset as a dataframe
-# Extract the NumPy array from the dataframe and convert the integer values to floating point values, 
-# which are more suitable for modeling with a neural network.
+"""
+Load the passenger dataset as a dataframe
+Extract the NumPy array from the dataframe and convert the integer values to floating point values, 
+which are more suitable for modeling with a neural network.
+"""
+
 dataframe = pd.read_csv('airline-passengers.csv', usecols=[1], engine='python')
 dataset = dataframe.values
 dataset = dataset.astype('float32')
 
+# Normalize the data 0-to-1
