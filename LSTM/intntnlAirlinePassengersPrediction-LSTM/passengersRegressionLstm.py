57008a3e38777d466b72bc6bea27efcd183cb419
okomojacob <jaylabs2030@gmail.com>
2021-03-13 12:11:25 +0300

    make predictions, more

--- LSTM/intntnlAirlinePassengersPrediction-LSTM/passengersRegressionLstm.py
+++ LSTM/intntnlAirlinePassengersPrediction-LSTM/passengersRegressionLstm.py
@@ -76,3 +76,19 @@ model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
 model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
 
+"""
+STEP 6: Make predictions
+"""
+
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+# invert predictions
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
